f278be21ba900e5244fa944124dd84948a70cce0
Paul Freeman <paul.freeman.cs@gmail.com>
2018-05-17 16:41:28 +1200

    more options and improved tracking

--- object_tracking/object_tracking.py
+++ object_tracking/object_tracking.py
@@ -2,11 +2,12 @@
 #pylint: disable=global-statement
 import argparse
 from math import sqrt
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-__version_info__ = (1, 0, 0)
+__version_info__ = (1, 0, 2)
 __version__ = '.'.join([str(n) for n in __version_info__])
 
 LICENSE_MSG = """\
@@ -20,13 +21,13 @@ PLOT_TITLE = 'Tracked Object Motion'
 PLOT_XLABEL = 'Time [seconds]'
 PLOT_YLABEL = 'Distance [metres]'
 
-
 def tracking():
     """Perform object tracking"""
     print(LICENSE_MSG)
     args = parse_args()
     try:
-        with open(args.output_file, 'xb') as output_file:
+        write_mode = 'wb' if args.force else 'xb'
+        with open(args.output_file, write_mode) as output_file:
             video = open_video(args.video_file)
             read_frame(video)
             print(SET_SCALE_MSG)
@@ -40,14 +41,21 @@ def tracking():
             press_enter()
             print('<object tracking window displayed>')
             bbox = select_bounding_box()
-            tracker = cv2.TrackerMIL_create()
+            if args.algorithm == 'KCF':
+                tracker = cv2.TrackerKCF_create()
+            elif args.algorithm == 'MIL':
+                tracker = cv2.TrackerMIL_create()
+            elif args.algorithm == 'Median-Flow':
+                tracker = cv2.TrackerMedianFlow_create()
+            else:
+                raise ValueError('Unknown algorithm type')
             if args.suppress_live_plot:
                 print(TRACKING_MSG)
             else:
                 print(TRACKING_MSG_W_PLOT)
             press_enter()
             print('<object tracking window displayed>')
-            points = track_video(video, tracker, bbox, scale, args.suppress_live_plot)
+            points = track_video(video, tracker, bbox, scale, args.suppress_live_plot, args.algorithm)
             np.save(output_file, np.asarray(points))
             print(LAST_PLOT_MSG)
             press_enter()
@@ -86,12 +94,13 @@ def get_scale_distance():
 
 def select_bounding_box():
     """Mark a bounding box to be tracked"""
-    bbox = cv2.selectROI(FRAME, False)
-    cv2.destroyAllWindows()
-    return bbox
+    try:
+        return cv2.selectROI(FRAME, False)
+    finally:
+        cv2.destroyAllWindows()
 
 
-def track_video(video, tracker, bbox, scale, suppress_live_plot):
+def track_video(video, tracker, bbox, scale, suppress_live_plot, algorithm):
     """Track a video"""
     fps = video.get(cv2.CAP_PROP_FPS)
     if not tracker.init(FRAME, bbox):
@@ -115,30 +124,32 @@ def track_video(video, tracker, bbox, scale, suppress_live_plot):
         except RuntimeError:
             break
         tracking_success, bbox = tracker.update(FRAME)
-        if tracking_success:
-            corner1 = int(bbox[0]), int(bbox[1])
-            corner2 = int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3])
-            distance = sqrt((((2.0 * bbox[0] + bbox[2]) / 2.0) - origin[0])**2
-                            + (((2.0 * bbox[1] + bbox[3]) / 2.0) - origin[1])**2)
-            time_points.append(frame_number / fps)
-            dist_points.append(distance / scale)
-            if not suppress_live_plot:
-                plt.cla()
-                plt.scatter(time_points, dist_points)
-                plt.title(PLOT_TITLE)
-                plt.xlabel(PLOT_XLABEL)
-                plt.ylabel(PLOT_YLABEL)
-                plt.pause(0.001)
-            cv2.rectangle(FRAME, corner1, corner2, (255, 0, 0), 2, 1)
-        else:
-            cv2.putText(
-                FRAME,
-                "Tracking failure detected",
-                (100, 80),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.75,
-                (0, 0, 255),
-                2)
+        if not tracking_success:
+            print(TRACKING_FAIL_MSG)
+            if algorithm == 'KCF':
+                tracker = cv2.TrackerKCF_create()
+            elif algorithm == 'MIL':
+                tracker = cv2.TrackerMIL_create()
+            elif algorithm == 'Median-Flow':
+                tracker = cv2.TrackerMedianFlow_create()
+            else:
+                raise ValueError('Unknown algorithm type')
+            bbox = select_bounding_box()
+            tracker.init(FRAME, bbox)
+        corner1 = int(bbox[0]), int(bbox[1])
+        corner2 = int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3])
+        distance = sqrt((((2.0 * bbox[0] + bbox[2]) / 2.0) - origin[0])**2
+                        + (((2.0 * bbox[1] + bbox[3]) / 2.0) - origin[1])**2)
+        time_points.append(frame_number / fps)
+        dist_points.append(distance / scale)
+        if not suppress_live_plot:
+            plt.cla()
+            plt.scatter(time_points, dist_points)
+            plt.title(PLOT_TITLE)
+            plt.xlabel(PLOT_XLABEL)
+            plt.ylabel(PLOT_YLABEL)
+            plt.pause(0.001)
+        cv2.rectangle(FRAME, corner1, corner2, (255, 0, 0), 2, 1)
         cv2.imshow("Tracking", FRAME)
         key_press = cv2.waitKey(1) & 0xff
         if key_press == ESCAPE_KEY:
@@ -180,14 +191,22 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='Perform OpenCV object tracking on a video file.')
     parser.add_argument(
+        '-a', '--algorithm',
+        help='the tracking algorithm to use',
+        choices=['KCF', 'MIL', 'Median-Flow'],
+        default='KCF')
+    parser.add_argument(
         'video_file',
         help='the video file containing the tracked object',
         metavar='VIDEO_FILE')
     parser.add_argument(
         '-o', '--output_file',
         help='output file into which to write NumPy data',
-        default='bigG_out.npy',
-        metavar='NUMPY_FILE')
+        default='bigG_out.npy')
+    parser.add_argument(
+        '-f', '--force',
+        action='store_true',
+        help='overwrite the output file if it already exists')
     parser.add_argument(
         '--suppress_live_plot',
         action='store_true',
@@ -215,12 +234,22 @@ ROI_BOX_MSG = """
 The first frame of the video will now be shown again. Please draw a box around
 the object (ROI) you want to track."""
 TRACKING_MSG = """
-Object tracking will now begin. This process may take some time."""
+Object tracking will now begin. If the tracker loses the object, it will pop up
+a new window for you to reselect the object. This may happen multiple times.
+The entire process may take some time."""
 TRACKING_MSG_W_PLOT = """
 Object tracking will now begin. A plot will update live with the tracked
 coordinates, taking into account both the scale and the frame rate of the video
-file. This process may take some time."""
+file. If the tracker loses the object, it will pop up a new window for you to
+reselect the object. This may happen multiple times. The entire process may
+take some time."""
+TRACKING_FAIL_MSG = """
+The tracker lost the object. Please draw a box around the object you are
+tracking"""
 LAST_PLOT_MSG = """
 Thanks for running the object tracking script. Your data points have been
 saved. I will plot the data one last time. Close the plot when you are ready to
 exit the program."""
+
+if __name__ == "__main__":
+    tracking()
